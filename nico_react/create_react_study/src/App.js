0b24bd5b621847ad56fe0916886dd15465367c06
websteak <ssk0080@gmail.com>
2022-08-09 20:04:38 +0900

    create react study

--- nico_react/create_react_study/src/App.js
+++ nico_react/create_react_study/src/App.js
@@ -1,7 +1,22 @@
+import Button from "./Button";
+import styles from './App.module.css'
+import { useEffect, useState } from 'react';
+
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState('');
+  const onClick = () => setValue(prev => prev + 1)
+  const onChange = () => { }
+  console.log('i run all the time')
+  useEffect(() => {
+    console.log('CALL THE API...')
+  }, [])
   return <>
     <div>
-      <h1>Welcome back!!!</h1>
+      <input onChange={onChange} type='text' placeholder='Search here...' />
+      <h1 className={styles.title}>{counter}</h1>
+      <Button text={counter} />
+      <button onClick={onClick}>{counter}</button>
     </div>
   </>
 }
